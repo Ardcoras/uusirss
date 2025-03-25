b1368a315d2fe0ebcc650218aba353bde5b3f6fa
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2025-03-25 04:14:38 +0200

    Add config for now

---
+++ config.py
@@ -0,0 +1,7 @@
+# Enable Flask's debugging features. Should be False in production
+ENV = 'development'
+DEBUG = True
+SECRET_KEY = 'ZMVq9Cws3KgjxbCtyLUW'
+SMTP_SERVER = 'localhost'
+FEEDBACK_TO = 'feedback@example.org'
+EMAIL_FROM = 'webmaster@example.org'

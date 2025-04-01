e5a0e2d4b6bd1b37ed314cf0d2c69075112a0ec4
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2025-04-01 20:28:41 +0300

    Fix the condition

--- app/views.py
+++ app/views.py
@@ -27,7 +27,7 @@ def get_rss():
     author = items['story']['author']['author_content']['firstname'] + ' ' + items['story']['author']['author_content']['lastname']
     itunes_item = iTunesItem(
       author = author,
-      image = 'https://uusijuttu.imgix.net/' + items['story']['social_image']['image']['url'] if 'social_image' in items['story'] else '',
+      image = 'https://uusijuttu.imgix.net/' + items['story']['social_image']['image']['url'] if items['story']['social_image'] else '',
       duration = items['story']['audio_length'],
       subtitle = items['story']['subhead'],
       summary = items['story']['story_content']['content'].get('audio_description', '')
